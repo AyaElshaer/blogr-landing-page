49fe92c49eae51edcecb844acffbc577de335cb7
aya446 <ayaramadan7799@gmail.com>
2022-03-21 18:02:51 +0200

    fix: header style

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 const navLinks = document.querySelector(".nav-items");
-const headerSec = document.querySelector(".header");
 const dropdowns = document.querySelectorAll(".ui.dropdown");
 const icon = document.querySelector(".icons");
-const links = document.querySelectorAll(".link");
-
-links.forEach( link => {
-  link.addEventListener("click", function () {
-    headerSec.classList.add("header-height");
-  });
- 
-});
 
 icon.addEventListener("click", function () {
   const hamburger = document.querySelector(".hamburger");
   const closeBtn = document.querySelector(".close");
+
   navLinks.classList.toggle("show");
   closeBtn.classList.toggle("show");
   hamburger.classList.toggle("hidde");
@@ -22,7 +14,6 @@ icon.addEventListener("click", function () {
     dropdown.classList.remove("floating");
     dropdown.classList.add("fluid");
   });
-  headerSec.classList.remove("header-height");
 });
 
 $(".ui.dropdown").dropdown();
